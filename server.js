d2c28cbf39d5306156e9c1aa294eb7adc55a4cba
kevinam99 <kevinam99@gmail.com>
2020-10-15 23:03:02 +0530

    Create four different queries

--- server.js
+++ server.js
@@ -1,1 +1,46 @@
 const app = require("express")()
+const expressGraphQL = require('express-graphql')
+const PORT = process.env.PORT || 5000
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString
+} = require('graphql')
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: "hello_world",
+        fields: () => ({
+            message: {
+                type: GraphQLString,
+                resolve: () => "Returning hello world"
+            },
+            field2: {
+                type: GraphQLString,
+                resolve: () => "Hello from field2"
+            },
+            field3: {
+                type: GraphQLString,
+                resolve: () => "Hello from field3"
+            },
+            field4: {
+                type: GraphQLString,
+                resolve: () => "Hello from field4"
+            },
+            
+        })
+    })
+})
+
+app.use('/graphql', expressGraphQL.graphqlHTTP(({
+    schema: schema,
+    graphiql: true
+})))
+
+app.listen(PORT, () => {
+    console.log(`Listening at port ${PORT}`)
+})
+
+app.get('/', (req, res) => {
+    res.send("Hello, Kevin! This is your GraphQL playground")
+})
